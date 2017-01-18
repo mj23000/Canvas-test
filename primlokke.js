c523f61e15ca81ee510142648c89c49d9e745b58
mj23000 <markusjacobsen@hotmail.com>
2017-01-18 10:59:11 +0100

    dd

--- primlokke.js
+++ primlokke.js
@@ -3,17 +3,19 @@ var ctx;
 var gammel=null;
 
 
-function init(){
+function init()
+{
 	var touchzone = document.getElementById("mycanvas");
 	touchzone.addEventListener("touchmove", opdaterCanvas, false);
 	touchzone.addEventListener("touchend", end, false);
 	ctx = touchzone.getContext("2d");
-
 }
 
-function opdaterCanvas(e){
+function opdaterCanvas(e)
+{
 	e.preventDefault();
-	if(gammel!=null){
+	if(gammel!=null)
+	{
 		ctx.beginPath();
 		ctx.moveTo(gammel.x, gammel.y);
 		ctx.lineTo(e.touches[0].pageX, e.touches[0].pageY);
@@ -22,7 +24,8 @@ function opdaterCanvas(e){
 	gammel={x:e.touches[0].pageX, y:e.touches[0].pageY};
 }
 
-function end(e){
+function end(e)
+{
 	e.preventDefault();
 	gammel=null;
 }
